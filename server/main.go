e665ffcb1d825b4193f3e7e798c2d453444cda83
Chirag Ghosh <cghosh828049@gmail.com>
2024-03-08 16:25:46 +0530

    feat: use sqlite3 as database
    Signed-off-by: Chirag Ghosh <cghosh828049@gmail.com>

--- server/main.go
+++ server/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/mattn/go-sqlite3"
 )
+
+var db *sql.DB
 
 func homeEndpoint(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
@@ -29,11 +34,23 @@ func heartbeatEndpoint(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	var err error
+	db, err = sql.Open("sqlite3", "/galaxy.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	http.HandleFunc("/home", homeEndpoint)
 	http.HandleFunc("/heartbeat", heartbeatEndpoint)
 
 	fmt.Println("Starting server on port 5000")
-	err := http.ListenAndServe(":5000", nil)
+	err = http.ListenAndServe(":5000", nil)
 	if errors.Is(err, http.ErrServerClosed) {
 		fmt.Printf("server closed\n")
 	} else if err != nil {
